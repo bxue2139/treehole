6cad65f34342c9c260468fd8a8d694a183a6a332
Ya <72751452+bxue2139@users.noreply.github.com>
2025-02-23 12:28:48 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 // config.php
 $servername = "localhost";
-$username   = "tree3";       // 数据库用户名
-$password   = "M3fmCwPYkX3eRjbF";           // 数据库密码
-$dbname     = "tree3";   // 数据库名
+$username   = "";       // 数据库用户名
+$password   = "";           // 数据库密码
+$dbname     = "";   // 数据库名
 
 // 创建数据库连接
 $conn = new mysqli($servername, $username, $password, $dbname);
